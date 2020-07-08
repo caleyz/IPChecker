b5045940cc474e4e3f4fc7d553fd6529bc1bf0fb
Zack Caley <zackcaleybusiness@gmail.com>
2020-07-08 19:32:52 +0100

    Initial Commit

---
+++ FlashWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPChecker
+{
+    public class FlashWindow
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public Int32 dwTimeout;
+        }
+
+        public const UInt32 FLASHW_ALL = 3;
+        public const UInt32 FLASHW_TIMERNOFG = 12;
+        public const UInt32 FLASHW_STOP = 0x00000004;
+
+        public void FlashWindowNow(IntPtr hWnd)
+        {
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+
+            FlashWindowEx(ref fInfo);
+        }
+
+        private static FLASHWINFO GetFLASHWINFO(UInt32 flashwConstant, UInt32 uCount = UInt32.MaxValue, UInt32 dwTimeout = 0)
+        {
+            FLASHWINFO fInfo = new FLASHWINFO
+            {
+                hwnd = Process.GetCurrentProcess().MainWindowHandle,
+                dwFlags = flashwConstant,
+                uCount = uCount
+            };
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            return fInfo;
+        }
+
+        public void StopFlash()
+        {
+            FLASHWINFO fInfo = GetFLASHWINFO(FLASHW_STOP);
+            FlashWindowEx(ref fInfo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace IPChecker
+{
+    public class Program
+    {
+        public static FlashWindow flashWindow;
+        public static string lastIP;
+
+        static void Main(string[] args)
+        {
+            InitTimer();
+            flashWindow = new FlashWindow();
+            AutomationFocusChangedEventHandler focusHandler = OnFocusChange;
+            Automation.AddAutomationFocusChangedEventHandler(focusHandler);
+            Automation.RemoveAutomationFocusChangedEventHandler(focusHandler);
+            lastIP = GetIP();
+            Application.Run();
+        }
+
+        public static void InitTimer()
+        {
+            Console.WriteLine("Starting Application");
+            Timer timer1;
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            int seconds = 30; //In Seconds
+            timer1.Interval = seconds * 1000;
+            timer1.Start();
+        }
+
+        public static string GetIP()
+        {
+            string ipV4 = NetworkInterface.GetAllNetworkInterfaces()
+                .Select(i => i.GetIPProperties().UnicastAddresses)
+                .SelectMany(u => u)
+                .Where(u => u.Address.AddressFamily == AddressFamily.InterNetwork)
+                .Select(i => i.Address)
+                .First() //I only need the first address as my primary network interface is the first one
+                .ToString();
+            return ipV4;
+        }
+
+        public static void OnFocusChange(object source, AutomationFocusChangedEventArgs e)
+        {
+            var focusedHandle = new IntPtr(AutomationElement.FocusedElement.Current.NativeWindowHandle);
+            var myConsoleHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+            if (focusedHandle == myConsoleHandle)
+            {
+                flashWindow.StopFlash();
+            }
+        }
+
+        public static void timer1_Tick(object sender, EventArgs e)
+        {
+            string currentIP = GetIP();
+            if (currentIP != lastIP)
+            {
+                Console.WriteLine("New IP: " + currentIP);
+                flashWindow.FlashWindowNow(Process.GetCurrentProcess().MainWindowHandle);
+            }
+        }
+    }
+}
